7b23bb1336e491b3aa97448c92f605668cf52124
Mihail Shmatok <>
2021-04-20 09:21:48 +0000

    1

--- main.py
+++ main.py
@@ -33,10 +33,10 @@ def check_number(checked_number):
     user_number = int(input (f"Too high.\n Guess again.\n You have {attempts} attempts remaining to guess the number.\n \n Make you guess:  "))
     return True
 
-number_guessed = True
-while  number_guessed:
-  number_guessed = check_number (user_number)
+number_unguessed = True
+while  number_unguessed:
+  number_unguessed = check_number (user_number)
   attempts -= 1
   if attempts == 0:
-    number_guessed = False
+    number_unguessed = False
     print (f"You've run out of guesses, you lose. The unguessed number is {rand_number}")
